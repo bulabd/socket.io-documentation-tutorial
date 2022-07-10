3ea95986525f9355040d8c60a2243377f4962a1c
bulabd <bulabd@outlook.com>
2022-07-10 11:31:04 -0400

    Create simple client-server connection with socket

---
+++ app.js
@@ -0,0 +1,18 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+  res.sendFile('/home/bulat/lighthouse/socketio-tutorial/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
+http.listen(3000, () => {
+  console.log('listening on PORT 3000');
+});
