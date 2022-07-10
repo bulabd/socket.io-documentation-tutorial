7e00d9d2969489b9e16405b643c48fd5cc2af170
bulabd <bulabd@outlook.com>
2022-07-10 12:21:59 -0400

    Learn namespacing

--- app.js
+++ app.js
@@ -6,36 +6,43 @@ app.get('/', (req, res) => {
   res.sendFile('/home/bulat/lighthouse/socketio-tutorial/index.html');
 });
 
+
+const nsp = io.of('/my-namespace');
+nsp.on('connection', (socket) => {
+  console.log('someone connected');
+  nsp.emit('hi', 'Hello everyone!');
+})
+
 let clients = 0;
 
-io.on('connection', (socket) => {
+// io.on('connection', (socket) => {
 
-  clients++;
+//   // clients++;
 
-  socket.emit('newclientconnect', { description: 'Hey, welcome!' });
-  socket.broadcast.emit('newclientconnect', { description: clients + ' clients connected!' });
+//   // socket.emit('newclientconnect', { description: 'Hey, welcome!' });
+//   // socket.broadcast.emit('newclientconnect', { description: clients + ' clients connected!' });
 
-  // io.sockets.emit('broadcast', { description: clients + ' clients connected!' });
+//   // io.sockets.emit('broadcast', { description: clients + ' clients connected!' });
 
-  // socket.on('clientEvent', (data) => {
-  //   console.log(data);
-  // });
+//   // socket.on('clientEvent', (data) => {
+//   //   console.log(data);
+//   // });
 
-  // setTimeout(() => {
-  //   // socket.send('Sent a message 4 seconds after connection!');
-  //   // socket.emit('testerEvent', { description: 'A custom event named testerEvent!' });
-  // }, 4000);
+//   // setTimeout(() => {
+//   //   // socket.send('Sent a message 4 seconds after connection!');
+//   //   // socket.emit('testerEvent', { description: 'A custom event named testerEvent!' });
+//   // }, 4000);
 
-  socket.on('disconnect', () => {
-    // console.log('A user disconnected');
+//   // socket.on('disconnect', () => {
+//   //   // console.log('A user disconnected');
 
-    clients--;
+//   //   clients--;
 
-    socket.broadcast.emit('newclientconnect', { description: clients + ' clients connected!' });
+//   //   socket.broadcast.emit('newclientconnect', { description: clients + ' clients connected!' });
 
-    // io.sockets.emit('broadcast', { description: clients + ' clients connected!' });
-  });
-});
+//   //   // io.sockets.emit('broadcast', { description: clients + ' clients connected!' });
+//   // });
+// });
 
 http.listen(3000, () => {
   console.log('listening on PORT 3000');
